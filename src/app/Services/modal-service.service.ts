07a91527013a1bbf441e3fc28b76489241e1bdea
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-02-28 10:26:06 +0200

    adding an Interface for the IModal and adding the functions to add Id for the modal service so that every component can call the service by the id provided for it and not call the same instance of the same object for multiple components

--- src/app/Services/modal-service.service.ts
+++ src/app/Services/modal-service.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from "@angular/core";
 
+
+interface IModal {
+  id: string;
+  Hidden: boolean;
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class ModalServiceService {
 
-  private Hidden : boolean = true;
+  private Modal : IModal[] = []  ;
   constructor() { }
 
-  togelVisibility(){
-    this.Hidden = !this.Hidden;
+  Register(id: string){
+    this.Modal.push({id: id, Hidden: true});
+    console.log(this.Modal);
   }
 
-  getVisibility() : boolean{
-    return this.Hidden;
+  togelVisibility(id : string){
+  let hiddenValue :boolean =  !!this.Modal.find(x => x.id === id)?.Hidden ;
+  this.Modal.find(x => x.id === id)!.Hidden = !hiddenValue;
+  }
+
+  getVisibility(id : string) : boolean{
+    return Boolean(this.Modal.find(x => x.id === id)?.Hidden);
   }
 }

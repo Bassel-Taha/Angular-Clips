9b490d92782d0b4e7d03ae72a0b7c7984043b7f7
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-02-29 09:04:31 +0200

    changing the tabs header and make it dynamic depending on the number of tabs in the tabs component and the "TabTitle prop (Input prop) "

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {AfterContentChecked, Component, ContentChildren, Input, QueryList} from '@angular/core';
+import {TabsComponent} from "../tabs/tabs.component";
 
 @Component({
   selector: 'app-tabs-container',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './tabs-container.component.html',
   styleUrl: './tabs-container.component.css'
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentChecked{
+
+  //adding an input to the tabs container component from the Tabs component to get the tab title
+
+  //contentChildren: QueryList<TabComponent>;
+  @ContentChildren(TabsComponent) tabs! : QueryList<TabsComponent>
+
+  constructor() {
+  }
+    ngAfterContentChecked(): void {
+
+
+    }
 
 }
--- src/app/shared/tabs/tabs.component.ts
+++ src/app/shared/tabs/tabs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './tabs.component.css'
 })
 export class TabsComponent {
-
+  @Input() TabTitle: string = '';
 }

4623f87cb47c0e4599826ad904ae3f3d1556accd
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-03-01 20:58:28 +0200

    creating the form group and form control for both the email and the password and adding the validators for both (But the type for the password is set in the DOM in the input tag) - adding the validators to the Dom and making it reactive to the input on inputting the data

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators, Form, ReactiveFormsModule} from "@angular/forms";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginFrom = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('',
+      [Validators.required,
+        Validators.minLength(6),
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{8,}$')
+    ])});
+
+
+  constructor() {
+
+  }
 }
+

8e5ba13e054f65be954e40f52818bdc158448efb
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-03-03 22:32:29 +0200

    adding the providers for the angularfire in the app.config because this project is a stand alone project

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAxrKQ8clZ9xXeUPaR0PvEL3-DUCG4tYeY",
+    authDomain: "angularclipps.firebaseapp.com",
+    projectId: "angularclipps",
+    storageBucket: "angularclipps.appspot.com",
+    messagingSenderId: "935714459232",
+    appId: "1:935714459232:web:f47e0e8d99a1b2ab91fd05",
+    measurementId: "G-2L2VPG5W5Q"
+  }
+}

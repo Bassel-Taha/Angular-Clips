046d0993a12d7ffcadd8d0861a287d03ee64c7e1
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-03-03 22:38:25 +0200

    to use the angular fire i have to import the angularfire modules in every separate module in the project because the project is atandalone one not a module based one

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgClass} from "@angular/common";
+import {Dismiss, DismissInterface, DismissOptions, InstanceOptions} from "flowbite";
+import {AngularFireAuth, AngularFireAuthModule} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    NgClass
+    NgClass,
+    AngularFireAuthModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  status : boolean|null = null;
 
   registerForm = new FormGroup({
     name: new FormControl('' ,[ Validators.required , Validators.minLength(3)]),
@@ -33,10 +38,91 @@ export class RegisterComponent {
       Validators.minLength(11),
       Validators.maxLength(11)])
   });
-  constructor() {
+  constructor(private Auth : AngularFireAuth){
   }
 
-  Register(){
-    console.log(this.registerForm.value);
+  async Register() {
+    try {
+      let userCredential = await this.Auth.createUserWithEmailAndPassword(this.registerForm.value.email as string, this.registerForm.value.password as string)
+      console.log(userCredential);
+      this.status = true;
+      this.toast();
+    } catch (e) {
+      console.log(e);
+      this.status = false;
+      this.toast();
+    }
+
+  }
+
+
+  toast() {
+
+    if (this.status === true) {
+      // target element that will be dismissed
+      const targetEl = document.getElementById('toast-danger ');
+      // optional trigger element
+      const triggerEl = document.getElementById('TasterDangerDismiss');
+      const options: DismissOptions = {
+        transition: 'transition-opacity',
+        duration: 1000,
+        timing: 'ease-out',
+
+        // callback functions
+        onHide: (context, targetEl) => {
+          console.log('element has been dismissed')
+          console.log(targetEl)
+        }
+      };
+
+      // instance options object
+      const instanceOptions: InstanceOptions = {
+        id: 'targetElement',
+        override: true
+      };
+
+      /*
+      * $targetEl (required)
+      * $triggerEl (optional)
+      * options (optional)
+      * instanceOptions (optional)
+      */
+      const dismiss: DismissInterface = new Dismiss(targetEl, triggerEl, options, instanceOptions);
+
+// programmatically hide it
+      dismiss.hide();
+    } else if (this.status === false) {
+      const targetEl = document.getElementById('toast-success ');
+
+      const triggerEl = document.getElementById('TasterSuccessDismiss');
+      const options: DismissOptions = {
+        transition: 'transition-opacity',
+        duration: 1000,
+        timing: 'ease-out',
+
+        // callback functions
+        onHide: (context, targetEl) => {
+          console.log('element has been dismissed')
+          console.log(targetEl)
+        }
+      };
+
+      // instance options object
+      const instanceOptions: InstanceOptions = {
+        id: 'targetElement',
+        override: true
+      };
+
+      /*
+      * $targetEl (required)
+      * $triggerEl (optional)
+      * options (optional)
+      * instanceOptions (optional)
+      */
+      const dismiss: DismissInterface = new Dismiss(targetEl, triggerEl, options, instanceOptions);
+
+      // programmatically hide it
+      dismiss.hide();
+    }
   }
 }

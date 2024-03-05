782d451aeb91130ab41ddac7cfa0f197de97b609
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-03-05 21:53:32 +0200

    using the isloggeedin observable in the nav module to show and hide the panels depending on the state of the User - using writing all the code once in the DOM by using the "async pipe which subscribe automaticly to the observable and get its value (also work on the promises to get its values )"

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {ModalServiceService} from "../Services/ModalService/modal-service.service";
+import {AuthService} from "../Services/AuthService/auth.service";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent implements OnInit{
-  constructor(public _ModalService: ModalServiceService) {
+  constructor(public _ModalService: ModalServiceService , public Auth : AuthService) {
   }
 
   ngOnInit(): void {

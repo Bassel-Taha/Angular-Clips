ed6295c2cde5b6c86b713fc448f9693da31e46d3
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-03-05 22:14:33 +0200

    there was a bug here the app is initializing the firebase only once due to making the if condition outside the onAuthChanged

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import firebase from 'firebase/compat/app';
+import {environment} from "./environments/environment";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+/////////////////////////// this is the main entry point for the application meaning that this is the first file that is executed when the application is started /////////////////////////
+
+// Initialize Firebase on the scale of the entire application
+firebase.initializeApp(environment.firebaseConfig);
+
+//that a variable to stop the application from being initialized more than once
+let isIntialized : boolean  = true;
+
+  // here we are using the onAuthStateChanged method to check if the user is logged in or not before initializing the application
+  // that means that the application will ask firebase to know if the user is logged in or not before initializing Angular
+  firebase.auth().onAuthStateChanged((user) => {
+    //check if the application is already initialized
+    if(isIntialized) {
+    bootstrapApplication(AppComponent, appConfig)
+      .catch((err) => console.error(err));
+      isIntialized = false;
+    }
+  });
+
+

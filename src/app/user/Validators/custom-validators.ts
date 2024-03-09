af0355547961d17650132b0c2a4b52efcc3d5bfa
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-03-09 14:06:04 +0200

    -using the setError functions to add an error to the matching control to be able to set the validators error message under the wanted input
    - the variables (matched and matching must be of the type abstract variable to be able to call the function setErrors)

--- src/app/user/Validators/custom-validators.ts
+++ src/app/user/Validators/custom-validators.ts
@@ -5,12 +5,18 @@ export class CustomValidators {
   static MatchingControls(Control : string , MatchingControl : string ):ValidatorFn
   {
     return (control: AbstractControl): ValidationErrors | null => {
-      let matcedControl = control.get(Control)?.value;
-      let matchingControl = control.get(MatchingControl)?.value
+      let matcedControl = control.get(Control);
+      let matchingControl = control.get(MatchingControl)
 
-      if ( matchingControl === '' || matchingControl == null) return {controlNotFound: false};
+      if (matchingControl == null) {
+        console.log("the matching control cant be found !")
+        return {controlNotFound: false};
+      }
 
-      if (matcedControl !== matchingControl) {
+      if (matcedControl?.value !== matchingControl.value) {
+        //adding an error to the matching control to be able to set the validators error message under the wanted input
+        matchingControl.setErrors({NoMatch : true})
+        // this return also return the error to the form group
         return {NoMatch: true};
       }
       return null;

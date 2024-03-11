520cdaf5fbbef5b01e720621421cd5f17dcbb61b
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-03-11 17:03:55 +0200

    using the routerlinkactive To bind a css class to the element when its activated in the url

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,14 +3,15 @@ import {ModalServiceService} from "../Services/ModalService/modal-service.servic
 import {AuthService} from "../Services/AuthService/auth.service";
 import {AsyncPipe} from "@angular/common";
 import {ToastService} from "../Services/Toast/toast.service";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
     AsyncPipe,
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'

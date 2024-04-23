e6ac1fe5079c3ff22e1a9ee0c356cd88d3005a57
Bassel taha <121397705+Bassel-Taha@users.noreply.github.com>
2024-04-23 11:58:13 +0200

    - creating the ffmpeg service and implementing the logic in it to load the ffmpeg asynchronously so that the app dosnt freeze while downloading the package and checking if the ffmpeg is already downloaded or not before loading it to prevent loading it again

---
+++ src/app/Services/FfmpegService/ffmpeg.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { createFFmpeg } from '@ffmpeg/ffmpeg';
+@Injectable({
+  providedIn: 'root'
+})
+export class FfmpegService {
+
+  // the boolean value to check if the ffmpeg is loaded and ready to be used or not
+  isReady : boolean = false;
+  private ffmpeg : any;
+  constructor() {
+    // creating the ffmpeg instance and setting the log to true to log all the activities of the ffmpeg to the console
+    this.ffmpeg = createFFmpeg({log: true});
+  }
+
+  // the async function to load the ffmpeg so that is dost freeze the app while downloading and checking if It's loaded or not before loading it
+  async Init ()
+  {
+    if (this.isReady === true)
+    {
+      return;
+    }
+    else
+    {
+      await this.ffmpeg.load()
+      this.isReady = true;
+    }
+  }
+}
